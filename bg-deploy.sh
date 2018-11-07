832f34eb29905aefca45ec9ede50d3cb7d91338d
Ahmet Ozturk <ahmet.ozturk@acedemand.com>
2018-11-07 23:06:16 +0300

    major changes

--- bg-deploy.sh
+++ bg-deploy.sh
@@ -1,23 +1,38 @@
 #!/bin/bash
 
-# bg-deploy.sh <servicename> <version> <green-deployment.yaml>
-# Deployment name should be <service>-<version>
+# bg-deploy.sh <app-name> <deployment.tmpl>
+set -eux
 
-DEPLOYMENTNAME=$1-$CI_COMMIT_SHA
-SERVICE=$1
+APP_NAME=$1
 VERSION=$CI_COMMIT_SHA
-DEPLOYMENTFILE=$3
+DEPLOYMENTFILE=$2
+NAMESPACE=web
 
-kubectl apply -f $DEPLOYMENTFILE
+cat $DEPLOYMENTFILE > deployment-$VERSION.yml
+
+sed -i "s/VERSION/$CI_COMMIT_SHA/g" deployment-$VERSION.yml
+sed -i "s/APP_NAME/$APP_NAME/g" deployment-$VERSION.yml
+
+kubectl apply -f deployment-$VERSION.yml
+
+DEPLOYMENTNAME="$(yq r deployment-$VERSION.yml -j | jq -r .metadata.name)"
+
+rm deployment-$VERSION.yml
 
 # Wait until the Deployment is ready by checking the MinimumReplicasAvailable condition.
-READY=$(kubectl get deploy $DEPLOYMENTNAME -n web -o json | jq '.status.conditions[] | select(.reason == "MinimumReplicasAvailable") | .status' | tr -d '"')
+READY=$(kubectl get deploy $DEPLOYMENTNAME -n $NAMESPACE -o json | jq '.status.conditions[] | select(.reason == "MinimumReplicasAvailable") | .status' | tr -d '"')
 while [[ "$READY" != "True" ]]; do
-    READY=$(kubectl get deploy $DEPLOYMENTNAME -n web -o json | jq '.status.conditions[] | select(.reason == "MinimumReplicasAvailable") | .status' | tr -d '"')
+    READY=$(kubectl get deploy $DEPLOYMENTNAME -n $NAMESPACE -o json | jq '.status.conditions[] | select(.reason == "MinimumReplicasAvailable") | .status' | tr -d '"')
     sleep 5
 done
 
-# Update the service selector with the new version
-kubectl patch svc -n web $SERVICE -p "{\"spec\":{\"selector\": {\"app\": \"${SERVICE}\", \"version\": \"${VERSION}\"}}}"
+if kubectl get svc -n $NAMESPACE $APP_NAME ; then
+    kubectl patch svc -n $NAMESPACE $APP_NAME -p "{\"spec\":{\"selector\": {\"app\": \"${APP_NAME}\", \"version\": \"${VERSION}\"}}}"
+    kubectl patch svc -n $NAMESPACE $APP_NAME -p "{\"metadata\":{\"labels\": {\"app\": \"${APP_NAME}\", \"version\": \"${VERSION}\"}}}"
+else
+    kubectl expose deployment $DEPLOYMENTNAME --type=NodePort --name=$APP_NAME -n $NAMESPACE
+fi
+
+for deployment in `kubectl get deployments -n $NAMESPACE | awk 'NR>1 {print $1}'`; do if [ "$deployment" != "$DEPLOYMENTNAME" ]; then kubectl delete deployment $deployment -n $NAMESPACE; fi; done
 
 echo "Done."
